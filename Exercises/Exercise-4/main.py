af6fe8e9b339a1fee6836737778d2b08b4a85dc4
Hung-DS <nguyenvanhung081005@gmail.com>
2025-05-06 15:09:04 +0700

    update bai 4

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,10 +1,47 @@
-import boto3
+import glob
+import json
+import csv
+import os
 
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], f'{name}{a}_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, f'{name}{i}_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+def json_to_csv(json_file_path):
+    with open(json_file_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    if isinstance(data, dict):
+        data = [data]  # Convert single dict to list of dicts
+
+    flattened_data = [flatten_json(record) for record in data]
+
+    if flattened_data:
+        csv_file_path = json_file_path.replace('.json', '.csv')
+        with open(csv_file_path, 'w', newline='', encoding='utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames=flattened_data[0].keys())
+            writer.writeheader()
+            writer.writerows(flattened_data)
 
 def main():
-    # your code here
-    pass
-
+    json_files = glob.glob('data/**/*.json', recursive=True)
+    for json_file in json_files:
+        print(f'Processing {json_file}...')
+        json_to_csv(json_file)
 
 if __name__ == "__main__":
     main()
